3aebfc32eac2d3ad4443a8dad69a0057fceaff14
Justas <justuxlt1@gmail.com>
2018-11-20 22:09:17 +0200

    fix

--- homework1/main.js
+++ homework1/main.js
@@ -1,31 +1,31 @@
-const minRows = 4;
-const maxRows = 8;
-
+const lineHeight = 15;
 let area = document.getElementById('textArea');
-area.addEventListener('keypress', resize);
-area.addEventListener('keydown', resize);
+area.addEventListener('input', resize);
 
 function resize() {
+    const minRows = 4;
+    const maxRows = 8;
+
     let obj = this;
     let key = event.keyCode || event.charCode;
     let currentScrollHeight = parseInt(obj.scrollHeight, 10);
-    let lines = Math.floor(currentScrollHeight / 15);
+    let lines = Math.floor(currentScrollHeight / lineHeight);
 
-    if (lines <= maxRows && lines > minRows && (key == 8 || key == 46)) {
-        setTimeout(() => {
-            obj.style.height = (lines - 1) * 15 + 4 + 'px';
-        }, 0);
-    } else if (lines < maxRows)
-        setTimeout(() => {
-            obj.style.height = 'auto';
-            obj.style.height = obj.scrollHeight + 'px';
-        }, 0);
+    const expandTextArea =
+		lines <= maxRows && lines > minRows && (key == 8 || key == 46);
+    if (expandTextArea) {
+        obj.style.height = (lines - 1) * lineHeight + 'px';
+    } else if (lines < maxRows) {
+        obj.style.height = 'auto';
+        obj.style.height = obj.scrollHeight + 'px';
+    }
 }
 function load() {
+    const minRows = 4;
     let area = document.getElementById('textArea');
     area.rows = minRows;
     area.style.height = area.scrollHeight + 'px';
     area.cols = 50;
-    area.style.lineHeight = '15px';
+    area.style.lineHeight = lineHeight + 'px';
 }
 document.onload = load();
